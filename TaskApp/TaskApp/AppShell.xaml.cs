52efc3126cb879c7c4c813e99816bc59a62d76f4
Junian Triajianto <author@junian.dev>
2025-02-13 12:35:38 +0700

    Init new project

---
+++ TaskApp/TaskApp/AppShell.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskApp.ViewModels;
+using TaskApp.Views;
+using Xamarin.Forms;
+
+namespace TaskApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
+
