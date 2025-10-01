67670bd826a3db2292d71225171e3ab2c2e054e2
Junian Triajianto <author@junian.dev>
2025-10-01 09:43:59 +0700

    Fix AudioPlayer

--- src/audio-player/AudioPlayer/App.xaml.cs
+++ src/audio-player/AudioPlayer/App.xaml.cs
@@ -1,29 +1,29 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace AudioPlayer
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
 
-			MainPage = new AudioPlayerPage();
-		}
+            MainPage = new AudioPlayerPage();
+        }
 
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart ()
+        {
+        }
 
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep ()
+        {
+        }
 
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume ()
+        {
+        }
+    }
 }
+
